5291c17b8969218a023110c3d35c2a9f4c4cc783
Compadre <fvh2012@gmail.com>
2023-09-27 18:17:38 -0500

    quuita comentario a  pip requirements

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # poetry install	Lo comenté por que no se usa en esta instalacion
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
